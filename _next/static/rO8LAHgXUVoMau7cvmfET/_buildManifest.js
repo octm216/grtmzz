11ab864f21588f3e30b3ac4f408acaf242ba1660
octm216 <163662424+octm216@users.noreply.github.com>
2024-06-09 22:36:43 -0900

    Add files via upload

---
+++ _next/static/rO8LAHgXUVoMau7cvmfET/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/921-90d79c8c440ca785.js","static/chunks/pages/index-3e6ad1a874792d7d.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
